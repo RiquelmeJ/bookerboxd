66d0c49e29bed879ab94a06f16223fa8fa4ea7c4
Leonardo <116222412+leopsdev@users.noreply.github.com>
2024-02-27 11:50:43 -0300

    add css de componentes

--- src/components/conteudo/conteudo.jsx
+++ src/components/conteudo/conteudo.jsx
@@ -1,4 +1,4 @@
-
+import "./Conteudo.css"
 
 function Conteudo() {
     return (<body>
@@ -6,27 +6,51 @@ function Conteudo() {
             <h1> Updates</h1>
         </header>
 
-        <div>
-            <img src="src/assets/gato.jpg" alt="" />
-            <h3> Pedro Pascal! avaliou A guerra dos Tronos</h3>
-            <img src="src/assets/guerra dos tronos.jpg" alt="" />
-            <p> O aspecto mais impressionante de "A Guerra dos Tronos" é a maneira como Martin
-                subverte as expectativas do leitor. Nenhum personagem está seguro e nenhum
-                evento é previsível. Isso cria uma sensação de urgência e imprevisibilidade que
-                mantém o leitor à beira do assento, ansioso para descobrir o que acontecerá a seguir.
-                Além disso, a prosa de Martin é fluida e envolvente, tornando fácil... Leia Mais</p>
-        </div>
+        <main id="conteudo-homepage">
 
-        <div>
-            <img src="src/assets/gato oculos.jpg" alt="" />
-            <h3> Paul Mescal! avaliou Harry Potter e a Pedra Filosofal</h3>
-            <img src="src/assets/harrypoter.webp" alt="" />
-            <p> O aspecto mais impressionante de "A Guerra dos Tronos" é a maneira como Martin
-                subverte as expectativas do leitor. Nenhum personagem está seguro e nenhum
-                evento é previsível. Isso cria uma sensação de urgência e imprevisibilidade que
-                mantém o leitor à beira do assento, ansioso para descobrir o que acontecerá a seguir.
-                Além disso, a prosa de Martin é fluida e envolvente, tornando fácil... Leia Mais</p>
-        </div>
+            <section id="avisos">
+                <p>Hello</p>
+            </section>
+
+            <section id="tweet">
+
+                <div id="titulo-tweet">
+
+                    <div id="foto-titulo">
+                        <img id="foto-perfil" src="src/assets/gato.jpg" alt="gato-perfil" />
+                        <h3 id="usuario-avaliou"> Pedro Pascal! avaliou A guerra dos Tronos</h3>
+                    </div>
+                    <p>13/02/24</p>
+
+                </div>
+
+                <div id="corpo-tweet">
+
+                    <a href="">
+                    <img id="foto-livro" src="src/assets/guerra dos tronos.jpg" alt="" />
+                    </a>
+                    
+
+                    <div id="texto-tweet">
+                        
+                        <h2>A Guerra dos Tronos - George R.R. Martin</h2>
+                        <h2>Avaliação</h2>
+                        <p> O aspecto mais impressionante de "A Guerra dos Tronos" é a maneira como Martin
+                            subverte as expectativas do leitor. Nenhum personagem está seguro e nenhum
+                            evento é previsível. Isso cria uma sensação de urgência e imprevisibilidade que
+                            mantém o leitor à beira do assento, ansioso para descobrir o que acontecerá a seguir.
+                            Além disso, a prosa de Martin é fluida e envolvente, tornando fácil... Leia Mais</p>
+
+                    </div>
+
+
+
+                </div>
+            </section>
+
+        </main>
+
+
 
     </body>)
 }
