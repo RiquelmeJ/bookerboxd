33ae37fc799dd79d40d0745bbc87ae2a23fe6310
Leonardo <116222412+leopsdev@users.noreply.github.com>
2024-02-26 21:18:57 -0300

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import "./Footer.css"
 
 function Footer() {
     return (<footer>
-        <div>
+        <div className="footer-div">
             <ul id="footer-nav">
                 <li><a href="">Sobre</a></li>
                 <li><a href="">Ajuda</a></li>
@@ -10,6 +10,16 @@ function Footer() {
                 <li><a href="">App</a></li>
             </ul>
             <p>@Feito por nós para a matéria de Web. Banco de dados: link aqui.</p>
+        </div>
+        <div>
+            <ul id="footer-img">
+                <li><img src="src/assets/instagram.svg" alt="instagram" /></li>
+                <li><img src="src/assets/tiktok.svg" alt="tiktok" /></li>
+                <li><img src="src/assets/facebook.svg" alt="facebook" /></li>
+                <li><img src="src/assets/youtube.svg" alt="youtube" /></li>
+                <li><img src="src/assets/x.svg" alt="twitter" /></li>
+                <li><img src="src/assets/github.svg" alt="github" /></li>
+            </ul>
         </div>
     </footer>)
 }
