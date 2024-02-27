646d38b87ae3256ce20148d2d711c6c304ae44fe
Riquelme Jatay <riquelmejatay@gmail.com>
2024-02-26 22:27:10 -0300

    Navbar updated (almost finished).

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,17 +3,29 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="flex">
-      <img id="navbar-logo" src="images/logo.svg" />
+      <a id="navbar-logo-id" href="">
+        <img id="navbar-logo" src="src/assets/logo.svg" />
+      </a>
       <span>
         <span className="flex" id="navbar-btns">
-          <a href="">Entrar</a>
-          <a href="">Sobre</a>
-          <input
-            id="src-bar"
-            name="src-bar"
-            type="text"
-            placeholder="Pesquisar títulos"
-          />
+          <a href="" className="navbar-link">Entrar</a>
+          <a href="" className="navbar-link">Criar uma conta</a>
+          <a href="" className="navbar-link">Sobre</a>
+          <div id="searchbar" className="flex">
+            <input
+              id="src-bar"
+              name="src-bar"
+              type="text"
+              placeholder="Pesquisar títulos"
+            />
+            <button id="src-btn">
+              <p className="sr-only">Pesquisar</p>
+              <img src="src/assets/lupa.svg"/>
+            </button>
+          </div>
+          <a id="user-btn" href="" className="flex">
+            Bem vindo(a),<em id="user-name">Visitante</em>.
+          </a>
         </span>
       </span>
     </nav>
