74a10d01698d0f1428e1d9b3f088c44639d9bff9
Leonardo <116222412+leopsdev@users.noreply.github.com>
2024-02-26 21:35:48 -0300

    fixed issues Conteudo e Navbar

--- src/components/conteudo/conteudo.jsx
+++ src/components/conteudo/conteudo.jsx
@@ -1,31 +1,34 @@
-function Content(){
 
-    <body> 
+
+function Conteudo() {
+    return (<body>
         <header>
             <h1> Updates</h1>
-            </header>
+        </header>
 
-            <div>
+        <div>
             <img src="src/assets/gato.jpg" alt="" />
             <h3> Pedro Pascal! avaliou A guerra dos Tronos</h3>
-            <img src="src/assets/guerra dos tronos.jpg" alt=""/>
+            <img src="src/assets/guerra dos tronos.jpg" alt="" />
             <p> O aspecto mais impressionante de "A Guerra dos Tronos" é a maneira como Martin
-                subverte as expectativas do leitor. Nenhum personagem está seguro e nenhum 
-                evento é previsível. Isso cria uma sensação de urgência e imprevisibilidade que 
+                subverte as expectativas do leitor. Nenhum personagem está seguro e nenhum
+                evento é previsível. Isso cria uma sensação de urgência e imprevisibilidade que
                 mantém o leitor à beira do assento, ansioso para descobrir o que acontecerá a seguir.
                 Além disso, a prosa de Martin é fluida e envolvente, tornando fácil... Leia Mais</p>
-            </div>
-            
-            <div>
+        </div>
+
+        <div>
             <img src="src/assets/gato oculos.jpg" alt="" />
             <h3> Paul Mescal! avaliou Harry Potter e a Pedra Filosofal</h3>
-            <img src="src/assets/harrypoter.webp" alt=""/>
+            <img src="src/assets/harrypoter.webp" alt="" />
             <p> O aspecto mais impressionante de "A Guerra dos Tronos" é a maneira como Martin
-                subverte as expectativas do leitor. Nenhum personagem está seguro e nenhum 
-                evento é previsível. Isso cria uma sensação de urgência e imprevisibilidade que 
+                subverte as expectativas do leitor. Nenhum personagem está seguro e nenhum
+                evento é previsível. Isso cria uma sensação de urgência e imprevisibilidade que
                 mantém o leitor à beira do assento, ansioso para descobrir o que acontecerá a seguir.
                 Além disso, a prosa de Martin é fluida e envolvente, tornando fácil... Leia Mais</p>
-            </div>
-            
-    </body>
+        </div>
+
+    </body>)
 }
+
+export default Conteudo
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,3 +12,5 @@ function Navbar(){
         </nav>
     );
 }
+
+export default Navbar
