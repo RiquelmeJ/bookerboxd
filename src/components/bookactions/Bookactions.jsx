5905837116bb17e9a5a733984205fe35e886aec9
Riquelme Jatay <riquelmejatay@gmail.com>
2024-02-28 13:34:13 -0300

    Photo changed.

--- src/components/bookactions/Bookactions.jsx
+++ src/components/bookactions/Bookactions.jsx
@@ -4,7 +4,7 @@ import { Rating } from '@mui/material';
 function Bookactions() {
     return (
         <div id="bookactions">
-            <img id="bookcover" src="https://m.media-amazon.com/images/I/8129HX+5JGL._AC_UF1000,1000_QL80_.jpg"/>
+            <img id="bookcover" src="src/assets/guerra dos tronos.jpg"/>
             <div id="actions">
                 <p><em>Quer ler essa obra?</em></p>
                 <button id="addToList">Adicionar à lista</button>
